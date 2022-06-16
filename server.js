bbccac46e208b029e959a21f5cc40a8b499e00c6
patrickarcher414 <patrickarcher414@gmail.com>
2022-06-15 21:39:15 -0500

    api post route, uniqid

--- server.js
+++ server.js
@@ -19,7 +19,32 @@ app.get('/notes', (req, res) => {
   res.sendFile(__dirname + '/notes.html');
 });
 
+// GET request for notes
 app.get('/api/notes', (req, res) => res.json(noteData));
+
+// POST request for notes
+app.post('/api/notes', (req, res) => {
+  
+  const { title, text } = req.body;
+    
+  if (title && text) {
+      
+    const newNote = {
+      title,
+      text,
+      note_id: uniqid()
+    };
+  
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+  
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
