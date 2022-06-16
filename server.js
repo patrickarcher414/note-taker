10297a8e8f1fb25693969ecbf4b1981cef42a1bc
patrickarcher414 <patrickarcher414@gmail.com>
2022-06-15 21:21:36 -0500

    api route for notes data

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
+const noteData = require('./db/db.json');
+
 const PORT = 3001;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -12,7 +19,7 @@ app.get('/notes', (req, res) => {
   res.sendFile(__dirname + '/notes.html');
 });
 
-
+app.get('/api/notes', (req, res) => res.json(noteData));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
